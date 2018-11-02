99d4e44e3d5452bba1128fc00b50ab52f21e0b8f
Thomas Struller-Baumann <contact@struller-baumann.de>
2018-11-02 14:32:43 +0100

    Update Top10WorstProcedures.sql

--- Top10WorstProcedures.sql
+++ Top10WorstProcedures.sql
@@ -1,13 +1,14 @@
-SELECT TOP 10
-         d.object_id
-        ,d.database_id
-        ,OBJECT_NAME(d.object_id, d.database_id) AS 'proc name'
-        ,d.cached_time
-        ,d.last_execution_time
-        ,d.total_elapsed_time
-        ,d.total_elapsed_time / d.execution_count AS avg_elapsed_time
-        ,d.last_elapsed_time
-        ,d.execution_count
-		    ,d.total_worker_time
-FROM     sys.dm_exec_procedure_stats AS d
+SELECT TOP(10)
+         ps.object_id
+        ,db.name AS db_name
+        ,OBJECT_NAME(ps.object_id, ps.database_id) AS proc_name
+        ,ps.cached_time
+        ,ps.last_execution_time
+        ,ps.total_elapsed_time
+        ,ps.total_elapsed_time / ps.execution_count AS avg_elapsed_time
+        ,ps.last_elapsed_time
+        ,ps.execution_count
+        ,ps.total_worker_time
+FROM     sys.dm_exec_procedure_stats AS ps
+  INNER JOIN sys.databases AS db ON db.database_id = ps.database_id
 ORDER BY avg_elapsed_time DESC;
