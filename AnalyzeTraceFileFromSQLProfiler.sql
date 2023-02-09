bb05448b285b01d5959f361b58491b831456ef64
Thomas Struller-Baumann <contact@struller-baumann.de>
2023-02-09 16:48:54 +0100

    Update AnalyzeTraceFileFromSQLProfiler.sql

--- AnalyzeTraceFileFromSQLProfiler.sql
+++ AnalyzeTraceFileFromSQLProfiler.sql
@@ -1,5 +1,5 @@
 -- Query and analyze a tracefile from SQL Profiler
 SELECT *
-FROM::fn_trace_gettable('D:\your_tracefile_from_sql_profiler.trc', DEFAULT)
+FROM::fn_trace_gettable('Path_to_your_tracefile_from_sql_profiler.trc', DEFAULT)
 ORDER BY Duration -- Or you can ORDER BY starttime etc.
 
